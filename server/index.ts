d29beb2873f43a5214210386bdd69467f629cc6e
Ronan Kian <mangubatronankian@gmail.com>
2025-06-19 21:58:18 +0800

    fix listen on 0.0.0.0

--- server/index.ts
+++ server/index.ts
@@ -59,8 +59,8 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen(port, '127.0.0.1', () => {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+  server.listen(port, '0.0.0.0', () => {
     log(`serving on port ${port}`);
   });
 })();
